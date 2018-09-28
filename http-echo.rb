d32575b22e45fc15e247f2bc1e84773db9ccc1e0
ChihChieh Huang <chihchieh_huang@htc.com>
2018-09-28 14:35:44 +0800

    replace nc by sinatra

--- http-echo.rb
+++ http-echo.rb
@@ -1,16 +1,47 @@
 #!/usr/bin/env ruby
+require 'sinatra'
+require 'yaml'
 
-body = ""
-flag = true
-while flag
-  line = gets
-  body << line
-  flag = false if line.strip.empty?
+set :bind, '0.0.0.0'
+set :port, ENV['HTTP_PORT']
+
+def self.route(*methods, path, &block)
+  methods.each do |method|
+    method.to_sym
+    self.send method, path, &block
+  end
 end
 
-puts 'HTTP/1.0 200 OK'
-puts 'Content-Type: text/plain'
-puts "Content-Length: #{body.length}"
-puts ''
+route :get, :post, :delete, :patch, :put, :head, :options, '/' do
+  content_type 'text/plain'
 
-puts body
+  if request.env['QUERY_STRING'] == ''
+    request_path = "#{request.env['REQUEST_PATH']}"
+  else
+    request_path = "#{request.env['REQUEST_PATH']}?#{request.env['QUERY_STRING']}"
+  end
+  request_method = request.env['REQUEST_METHOD']
+  http_version = request.env['HTTP_VERSION']
+
+  headers = Hash.new
+  header_keys = request.env.keys.select {|k| k[/^HTTP_.*$/] }.reject {|k| k == 'HTTP_VERSION'}
+  header_keys.each do |key|
+    tokens = key.split("_")[1..-1].map {|t| t.capitalize! }
+    headers[key] = tokens.join("-")
+  end
+
+  header_keys = ['CONTENT_TYPE', 'CONTENT_LENGTH']
+  header_keys.each do |key|
+    tokens = key.split("_").map {|t| t.capitalize! }
+    headers[key] = tokens.join("-")
+  end
+
+  return_value = Array.new
+  return_value.push "#{request_method} #{request_path} #{http_version}"
+  headers.each do |key, header|
+    return_value.push "#{header}: #{request.env[key]}"
+  end
+  return_value.push ''
+
+  return_value.join("\n")
+end
